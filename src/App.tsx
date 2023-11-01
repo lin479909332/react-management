000f60eddb25a9ebf2d28f85442ae812e1fad094
lin479909332 <lin479909332@gmail.com>
2023-11-01 23:53:43 +0800

    feat(App.tsx): 展示路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
+import { useRoutes } from 'react-router-dom'
+import router from './router'
+
 function App() {
-  return <div className="App">
-    顶级组件
-  </div>
+  const outlet = useRoutes(router)
+  return (
+    <div className="App">
+      {/* 顶级组件 */}
+      {outlet}
+    </div>
+  )
 }
 
 export default App
