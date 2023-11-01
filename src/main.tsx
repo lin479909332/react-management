66575bb5da755872454750f1ecbc552c30261624
lin479909332 <lin479909332@gmail.com>
2023-11-01 22:50:53 +0800

    feat(main.tsx): 修改全局样式的引入方式

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 // 初始化样式
 import 'reset-css'
 // 全局样式
-import './assets/styles/global.scss'
+import '@/assets/styles/global.scss'
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
