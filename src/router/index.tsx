98c363bc6dec21f88b9ee908732ac80b73fd4510
lin479909332 <lin479909332@gmail.com>
2023-11-05 16:21:17 +0800

    feat(router/index.tsx): 新增一条Page301的路由和重定向路由

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -10,6 +10,9 @@ const Page1: React.LazyExoticComponent<React.ComponentType<any>> = lazy(
 const Page2: React.LazyExoticComponent<React.ComponentType<any>> = lazy(
   () => import('../views/Page2'),
 )
+const Page301: React.LazyExoticComponent<React.ComponentType<any>> = lazy(
+  () => import('../views/Page301'),
+)
 
 const withLoadingComponenet = (comp: JSX.Element) => (
   <React.Suspense fallback={<div>Loading...</div>}>{comp}</React.Suspense>
@@ -32,7 +35,15 @@ const routes = [
         path: '/page2',
         element: withLoadingComponenet(<Page2 />),
       },
+      {
+        path: '/page3/page301',
+        element: withLoadingComponenet(<Page301 />),
+      },
     ],
+  },
+  {
+    path: '*',
+    element: <Navigate to="/page1" />,
   },
 ]
 
