caf75d8846f086e261032f36a730a83c939acdb3
lin479909332 <lin479909332@gmail.com>
2023-11-05 15:08:18 +0800

    feat(router/index.tsx): 新增两个子路由

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,21 +1,38 @@
-// import React, { lazy } from 'react'
+import React, { lazy } from 'react'
 // Navigate重定向组件
 import { Navigate } from 'react-router-dom'
 
 import Home from '../views/Home'
 
-// const withLoadingComponenet = (comp: JSX.Element) => (
-//   <React.Suspense fallback={<div>Loading...</div>}>{comp}</React.Suspense>
-// )
+const Page1: React.LazyExoticComponent<React.ComponentType<any>> = lazy(
+  () => import('../views/Page1'),
+)
+const Page2: React.LazyExoticComponent<React.ComponentType<any>> = lazy(
+  () => import('../views/Page2'),
+)
+
+const withLoadingComponenet = (comp: JSX.Element) => (
+  <React.Suspense fallback={<div>Loading...</div>}>{comp}</React.Suspense>
+)
 
 const routes = [
   {
     path: '/',
-    element: <Navigate to="/home" />,
+    element: <Navigate to="/page1" />,
   },
   {
-    path: '/home',
+    path: '/',
     element: <Home />,
+    children: [
+      {
+        path: '/page1',
+        element: withLoadingComponenet(<Page1 />),
+      },
+      {
+        path: '/page2',
+        element: withLoadingComponenet(<Page2 />),
+      },
+    ],
   },
 ]
 
