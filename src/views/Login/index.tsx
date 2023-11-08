b47b00334c143514fa47f09fce5822196f9bfa76
lin479909332 <lin479909332@gmail.com>
2023-11-08 23:40:41 +0800

    feat(Login/index.tsx): 获取用户名密码和验证码，添加登录按钮事件

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,15 +1,38 @@
-import { useEffect } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import { Input, Space, Button } from 'antd'
 import styles from './login.module.scss'
 import initLoginBg from './init.ts'
 import './loginBox.scss'
 const view = () => {
+  // 初始化背景
   useEffect(() => {
     initLoginBg()
     window.onresize = function () {
       initLoginBg()
     }
   }, [])
+  // 用户名
+  const [usernameVal, setUsernameVal] = useState('')
+  // 密码
+  const [passwordVal, setPasswordVal] = useState('')
+  // 验证码
+  const [captchaVal, setCaptchaVal] = useState('')
+  // 用户名输入事件
+  const usernameChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setUsernameVal(e.target.value)
+  }
+  // 密码输入事件
+  const passwordChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setPasswordVal(e.target.value)
+  }
+  // 验证码输入事件
+  const captchaChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setCaptchaVal(e.target.value)
+  }
+  // 点击登录按钮事件
+  const gotoLogin = () => {
+    console.log(usernameVal, passwordVal, captchaVal)
+  }
   return (
     <div className={styles.loginPage}>
       {/* 存放背景 */}
@@ -24,10 +47,10 @@ const view = () => {
         {/* 表单部分 */}
         <div className="form">
           <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
-            <Input placeholder="用户名" />
-            <Input.Password placeholder="密码" />
+            <Input placeholder="用户名" onChange={usernameChange} />
+            <Input.Password placeholder="密码" onChange={passwordChange} />
             <div className="captchaBox">
-              <Input placeholder="验证码" />
+              <Input placeholder="验证码" onChange={captchaChange} />
               <div className="captchaImg">
                 <img
                   height="38"
@@ -36,7 +59,7 @@ const view = () => {
                 />
               </div>
             </div>
-            <Button type="primary" className="loginBtn" block>
+            <Button type="primary" className="loginBtn" block onClick={gotoLogin}>
               登录
             </Button>
           </Space>
