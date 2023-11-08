dd549abeaa0735c1d25db95cc16b4bf2c364c0ac
lin479909332 <lin479909332@gmail.com>
2023-11-08 23:07:28 +0800

    feat(Login/index.tsx): 新增表单元素和自定义样式

--- src/views/Login/index.tsx
+++ src/views/Login/index.tsx
@@ -1,6 +1,8 @@
 import { useEffect } from 'react'
+import { Input, Space, Button } from 'antd'
 import styles from './login.module.scss'
 import initLoginBg from './init.ts'
+import './loginBox.scss'
 const view = () => {
   useEffect(() => {
     initLoginBg()
@@ -13,10 +15,21 @@ const view = () => {
       {/* 存放背景 */}
       <canvas id="canvas" style={{ display: 'block' }}></canvas>
       {/* 登录盒子 */}
-      <div className={styles.loginBox}>
+      <div className={styles.loginBox + ' loginBox'}>
+        {/* 标题部分 */}
         <div className={styles.title}>
-          <h1>React+Ts&nbsp;·&nbsp;通用后台系统</h1>
+          <h1>React+TS&nbsp;·&nbsp;通用后台系统</h1>
           <p>Strive Everyday</p>
+        </div>
+        {/* 表单部分 */}
+        <div className="form">
+          <Space direction="vertical" size="middle" style={{ display: 'flex' }}>
+            <Input placeholder="用户名" />
+            <Input.Password placeholder="密码" />
+            <Button type="primary" block>
+              登录
+            </Button>
+          </Space>
         </div>
       </div>
     </div>
