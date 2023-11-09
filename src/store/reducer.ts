1a662b2598a5e857c0a802ffa3620b44dd99232d
lin479909332 <lin479909332@gmail.com>
2023-11-09 23:04:58 +0800

    feat(store/reducer.ts): 新增两个action

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,9 +1,19 @@
 const defaultState = {
   num: 20,
 }
-let reducer = (state = defaultState) => {
+let reducer = (state = defaultState, action: { type: string; val: number }) => {
   let newState = JSON.parse(JSON.stringify(state))
 
+  switch (action.type) {
+    case 'add1':
+      newState.num++
+      break
+    case 'add2':
+      newState.num += action.val
+      break
+    default:
+      break
+  }
   return newState
 }
 
