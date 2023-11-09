458424d978687be12359a04e2e6bc2fc70b3bc49
lin479909332 <lin479909332@gmail.com>
2023-11-09 23:05:38 +0800

    feat(Page1.tsx): 读取redux的数据并调用方法

--- src/views/Page1.tsx
+++ src/views/Page1.tsx
@@ -1,5 +1,20 @@
+import { useSelector, useDispatch } from 'react-redux'
 const Page1 = () => {
-  return <div>我是Page1</div>
+  const { num } = useSelector((state) => ({
+    num: state.num,
+  }))
+  const dispatch = useDispatch()
+  const changeNum = () => {
+    // dispatch({ type: 'add1' })
+    dispatch({ type: 'add2', val: 10 })
+  }
+  return (
+    <div>
+      <h1>我是Page1</h1>
+      <p>{num}</p>
+      <button onClick={changeNum}>按钮</button>
+    </div>
+  )
 }
 
 export default Page1
