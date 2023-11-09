08662e9386082ae40d46944e034e7085cace3356
lin479909332 <lin479909332@gmail.com>
2023-11-09 23:59:35 +0800

    feat(Page.tsx): 数据修改为抽离reducer后的写法

--- src/views/Page1.tsx
+++ src/views/Page1.tsx
@@ -1,22 +1,37 @@
 import { useSelector, useDispatch } from 'react-redux'
 const Page1 = () => {
   interface AppState {
-    num: number
+    NumStatusReducer: {
+      num: number
+    }
+    SarrStatusReducer: {
+      sarr: number[]
+    }
   }
-  const { num } = useSelector((state: AppState) => state)
-  // const { num } = useSelector((state) => ({
-  //   num: state.num,
-  // }))
+  // const { num } = useSelector((state: AppState) => state.NumStatusReducer)
+  const { num } = useSelector((state: AppState) => ({
+    num: state.NumStatusReducer.num,
+  }))
+
+  const { sarr } = useSelector((state: AppState) => ({
+    sarr: state.SarrStatusReducer.sarr,
+  }))
   const dispatch = useDispatch()
   const changeNum = () => {
     // dispatch({ type: 'add1' })
     dispatch({ type: 'add2', val: 10 })
   }
+
+  const changeSarr = () => {
+    dispatch({ type: 'sarrPush', val: 100 })
+  }
   return (
     <div>
       <h1>我是Page1</h1>
       <p>{num}</p>
+      <p>{sarr}</p>
       <button onClick={changeNum}>按钮</button>
+      <button onClick={changeSarr}>数组按钮</button>
     </div>
   )
 }
