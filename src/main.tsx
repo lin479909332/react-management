ac8267f763b07cdbd474f1ceaf4d0b1c5c9fa3ed
lin479909332 <lin479909332@gmail.com>
2023-11-09 22:35:55 +0800

    feat(main.tsx): 关联仓库

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,17 @@ import 'reset-css'
 // 全局样式
 import '@/assets/styles/global.scss'
 import { BrowserRouter } from 'react-router-dom'
+// 状态管理
+import { Provider } from 'react-redux'
+import store from '@/store/index.ts'
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
 )
