1366a19d47b5fa9ddef8a5d77e868deb3400b7ad
lin479909332 <lin479909332@gmail.com>
2023-11-09 23:58:18 +0800

    feat(store/index.ts): 合并reducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,18 @@
-import {legacy_createStore} from 'redux'
-import reducer from "./reducer.ts"
-// 创建仓库
+import { legacy_createStore, combineReducers } from 'redux'
 
+import NumStatusReducer from './NumStatus/reducer'
+import SarrStatusReducer from './SarrStatus/reducer'
+
+// 合并reducer
+const reducers = combineReducers({
+  NumStatusReducer,
+  SarrStatusReducer,
+})
+// 创建仓库
 // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()让浏览器redux-dev-tools能正常使用插件
-const store = legacy_createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ &&
-  window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = legacy_createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+)
 
 export default store
