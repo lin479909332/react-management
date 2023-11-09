83814369628d1324b9fda587de3b6ea9ee4f17c4
lin479909332 <lin479909332@gmail.com>
2023-11-09 23:04:32 +0800

    feat(store/index.ts): 引入配置项让浏览器可以正常使用插件

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,9 @@
 import {legacy_createStore} from 'redux'
 import reducer from "./reducer.ts"
 // 创建仓库
-const store = legacy_createStore(reducer)
+
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()让浏览器redux-dev-tools能正常使用插件
+const store = legacy_createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ &&
+  window.__REDUX_DEVTOOLS_EXTENSION__())
 
 export default store
