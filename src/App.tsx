53ba7cb6921830d00c8627cece515c50fcf0d6fd
lin479909332 <lin479909332@gmail.com>
2023-11-11 23:33:21 +0800

    feat(App.tsx): 实现前置路由守卫

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,46 @@
-import { useRoutes } from 'react-router-dom'
+import { useEffect } from 'react'
+import { useRoutes, useLocation, useNavigate } from 'react-router-dom'
+import { message } from 'antd'
 import router from './router'
 
-function App() {
+const navigateTo = useNavigate()
+// 去往登录页
+function ToLogin() {
+  useEffect(() => {
+    navigateTo('/login')
+    message.warning('您还没有登录，请先登录后再访问！')
+  }, [])
+  return <div></div>
+}
+// 去往首页
+function ToPage1() {
+  useEffect(() => {
+    navigateTo('/page1')
+    message.warning('您已经登录过了！')
+  }, [])
+  return <div></div>
+}
+
+function BeforeRouteEnter() {
   const outlet = useRoutes(router)
+  const token = localStorage.getItem('react-management-token')
+  const location = useLocation()
+  // 登录过了有token 想跳转到登录页 => 让用户跳转到首页
+  if (location.pathname === '/login' && token) {
+    return <ToPage1 />
+  }
+  // 还没登录没有token 想跳转到首页 => 让用户跳转到登录页
+  if (location.pathname === '/page1' && !token) {
+    return <ToLogin />
+  }
+  return outlet
+}
+
+function App() {
   return (
     <div className="App">
       {/* 顶级组件 */}
-      {outlet}
+      <BeforeRouteEnter />
     </div>
   )
 }
