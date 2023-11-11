5bc9371d88e50edd0ab6311a6b20b33b37948bce
lin479909332 <lin479909332@gmail.com>
2023-11-11 23:05:57 +0800

    feat(api api.d.ts): 新增登录api和所需类型约束

--- src/request/api.ts
+++ src/request/api.ts
@@ -1,3 +1,8 @@
 import request from './index'
 
+// 验证码请求
 export const captchaAPI = (): Promise<CaptchaAPIRes> => request.get('/prod-api/captchaImage')
+
+// 登录请求
+export const loginAPI = (params: LoginAPIReq): Promise<LoginAPIRes> =>
+  request.get('/prod-api/login', params)
