d3e6c565b34b9def0ac42911f8d0dfede518d2b3
lin479909332 <lin479909332@gmail.com>
2023-11-11 21:41:09 +0800

    perf(NumStatus/reducer.ts): 删除掉无用形参val

--- src/store/NumStatus/reducer.ts
+++ src/store/NumStatus/reducer.ts
@@ -1,6 +1,6 @@
 import handleNum from './index'
 
-let reducer = (state = { ...handleNum.state }, action: { type: string; val: number }) => {
+let reducer = (state = { ...handleNum.state }, action: { type: string }) => {
   let newState = JSON.parse(JSON.stringify(state))
 
   // switch (action.type) {
@@ -13,7 +13,7 @@ let reducer = (state = { ...handleNum.state }, action: { type: string; val: numb
   //   default:
   //     break
   // }
-  
+
   // 拿着action.type和actionNames进行每一项的对比，如果是相等，就调用 模块名.actions[下标](newState,action)
   for (let key in handleNum.actionNames) {
     if (action.type === handleNum.actionNames[key]) {
